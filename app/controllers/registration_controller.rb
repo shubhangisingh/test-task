cda6afe88fb558f1ff3a0129faad55d4bed03028
Your Name <shubhangi.mca09@gmail.com>
2017-08-17 01:37:43 +0530

    adding devise

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/registrations_controller.rb
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
+  def create
+    # add custom create logic here
+  end
+
+  def update
+    super
+  end
+end 
